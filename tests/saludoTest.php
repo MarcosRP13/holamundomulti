96faf398b43e662754cf3a7b420e1c7f39f68c1e
marope01 <marope01@alumnatflorida.es>
2026-01-20 13:11:23 +0100

    Primer buen mejor sshhpuer commit - despliegue automático

--- tests/saludoTest.php
+++ tests/saludoTest.php
@@ -8,16 +8,12 @@ class SaludoTest extends TestCase
         $saludo = new Saludo();
         $resultado = $saludo->generarSaludoAleatorio();
 
-        // Verifica que el resultado es un string
         $this->assertIsString($resultado['saludo']);
 
-        // Verifica que el resultado contiene un saludo válido
         $this->assertNotEmpty($resultado['saludo']);
 
-        // Verifica que el código también es un string
         $this->assertIsString($resultado['codigo']);
 
-        // Verifica que el código no esté vacío
         $this->assertNotEmpty($resultado['codigo']);
     }
 }
